0f874d1fbd2927eaafc887defab6d40ff0ae78da
Erick Clinton <erickleite2009321@gmail.com>
2025-01-22 13:07:48 -0300

    start project with dependencies

---
+++ src/main/java/br/com/barber/jhow/BarberJhowApplication.java
@@ -0,0 +1,13 @@
+package br.com.barber.jhow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberJhowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberJhowApplication.class, args);
+	}
+
+}
