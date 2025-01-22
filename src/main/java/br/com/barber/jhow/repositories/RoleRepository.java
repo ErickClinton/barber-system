f1b33f06e0cecb2ad773f27eeca8e7dd095fa0ba
Erick Clinton <erickleite2009321@gmail.com>
2025-01-22 16:57:56 -0300

    feat: create findbytype

--- src/main/java/br/com/barber/jhow/repositories/RoleRepository.java
+++ src/main/java/br/com/barber/jhow/repositories/RoleRepository.java
@@ -1,7 +1,11 @@
 package br.com.barber.jhow.repositories;
 
 import br.com.barber.jhow.entities.RoleEntity;
+import br.com.barber.jhow.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+     Optional<RoleEntity> findByType(RoleEnum type);
 }
