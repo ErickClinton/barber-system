bd40abf88ca853b784514192905f12d91817c4d1
Erick Clinton <erickleite2009321@gmail.com>
2025-01-22 16:57:25 -0300

    feat: create routes

--- src/main/java/br/com/barber/jhow/controller/UserController.java
+++ src/main/java/br/com/barber/jhow/controller/UserController.java
@@ -1,25 +1,40 @@
 package br.com.barber.jhow.controller;
 
+import br.com.barber.jhow.controller.dto.LoginRequest;
+import br.com.barber.jhow.controller.dto.LoginResponse;
+import br.com.barber.jhow.controller.dto.SignRequest;
 import br.com.barber.jhow.entities.UserEntity;
 import br.com.barber.jhow.repositories.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.barber.jhow.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
 
-    private final UserRepository userEntity;
+    private final UserService userService;
+    private final JwtEncoder jwtEncoder;
 
-    public UserController(UserRepository userEntity) {
-        this.userEntity = userEntity;
+    public UserController(UserService userService, JwtEncoder jwtEncoder) {
+        this.userService = userService;
+        this.jwtEncoder = jwtEncoder;
     }
 
-    @GetMapping()
-    public String teste(){
-        var newUser = new UserEntity();
-        newUser.setName("John Doe");
-        return "d";
+    @PostMapping(path = "/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        var token = this.userService.login(loginRequest);
+        System.out.println(token.accessToken());
+        return ResponseEntity.ok(token);
     }
+
+    @PostMapping(path = "/sign")
+    public ResponseEntity<Void> login(@RequestBody SignRequest signRequest) {
+        var user = this.userService.createUser(signRequest);
+        return ResponseEntity.created(URI.create("/users/"+ user.getId().toString())).build();
+    }
+
 }
