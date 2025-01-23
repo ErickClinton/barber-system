b46d9f724f5c29b56c9e54aa34fb2f9de0649446
Erick Clinton <erickleite2009321@gmail.com>
2025-01-23 13:06:03 -0300

    feat: valid request

--- src/main/java/br/com/barber/jhow/controller/UserController.java
+++ src/main/java/br/com/barber/jhow/controller/UserController.java
@@ -5,8 +5,11 @@ import br.com.barber.jhow.controller.dto.LoginResponse;
 import br.com.barber.jhow.controller.dto.SignRequest;
 import br.com.barber.jhow.entities.UserEntity;
 import br.com.barber.jhow.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -23,14 +26,26 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<?> teste(@AuthenticationPrincipal Jwt jwt) {
+
+
+        String email = jwt.getClaimAsString("sub");
+        String scope = jwt.getClaimAsString("scope");
+        System.out.println("Usuário autenticado: " + email);
+        System.out.println("Escopo: " + scope);
+
+        return ResponseEntity.ok("Usuário autenticado: " + scope);
+    }
+
     @PostMapping(path = "/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         var token = this.userService.login(loginRequest);
         return ResponseEntity.ok(token);
     }
 
     @PostMapping(path = "/sign")
-    public ResponseEntity<Void> login(@RequestBody SignRequest signRequest) {
+    public ResponseEntity<Void> login(@RequestBody @Valid SignRequest signRequest) {
         var user = this.userService.createUser(signRequest);
         return ResponseEntity.created(URI.create("/users/"+ user.getId().toString())).build();
     }
