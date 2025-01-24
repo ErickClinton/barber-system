1145fe6fa1aedd7c9547b642c8a96e4f6e8dc8ed
Erick Clinton <erickleite2009321@gmail.com>
2025-01-24 12:06:03 -0300

    feat:create in repository for find schedule by user id

--- src/main/java/br/com/barber/jhow/repositories/SchedulingRepository.java
+++ src/main/java/br/com/barber/jhow/repositories/SchedulingRepository.java
@@ -1,11 +1,16 @@
 package br.com.barber.jhow.repositories;
 
 import br.com.barber.jhow.entities.SchedulingEntity;
+import br.com.barber.jhow.repositories.dto.SchedulingView;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SchedulingRepository extends JpaRepository<SchedulingEntity, Integer> {
     List<SchedulingEntity> findByBarberId(UUID id);
+    Page<SchedulingView> findByUserId(UUID id, PageRequest pageable);
 }
